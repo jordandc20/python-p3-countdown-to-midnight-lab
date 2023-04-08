e7b710bfd5714e3e8165d2e12b22332247a6fdce
Diana Jordan <dianacjordan@gmail.com>
2023-04-08 18:11:25 -0400

    coplete lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
-# your code goes here!
+def countdown(integer):
+    number=integer
+    while number >0:
+        print( f'{number} SECOND(S)!')
+        number-=1
+    print('HAPPY NEW YEAR!')
+
+import time    
+def countdown_with_sleep(integer):
+    number=integer
+    while number >0:
+        print( f'{number} SECOND(S)!')
+        number-=1
+        time.sleep(1)
+    print('HAPPY NEW YEAR!')    
+    
+countdown(15)
